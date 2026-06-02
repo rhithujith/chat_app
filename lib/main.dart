d9a121c0b8cd718cb92dfe9f17b7c702bd5d64b1
rhithujith <rhithujithp@gmail.com>
2026-06-02 14:37:55 +0530

    Persist user login sessions on app launch by checking FirebaseAuth.instance.currentUser in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'login_screen.dart';
+import 'contacts_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,9 +18,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Check if a user is already signed in via Firebase Auth
+    final user = FirebaseAuth.instance.currentUser;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
+      home: user != null ? const ContactsScreen() : const LoginScreen(),
     );
   }
 }
